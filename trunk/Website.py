d5e535ff322bd36e798c127d5a93220957e5ba1e
robertlayton <robertlayton@1176ff72-061f-11df-bdae-5552c88b53c9>
2010-01-22 00:17:36 +0000

    Website properties can now include references to other properties. This will be included at compile time using Website.compileProperty()

--- trunk/Website.py
+++ trunk/Website.py
@@ -8,9 +8,11 @@
     for an entire website, to enable things like standard lists, etc.
     Things like relative links can be expanded easily as well, if the user wishes.
 """
-import os
+import os, re
 
 from WebsiteDefaults import defaults
+
+single_br_pattern = re.compile("{[\w]+}")
 
 class Website(object):
     """ Contains information about the website and returns consistent HTML Objects
@@ -23,23 +25,35 @@ class Website(object):
         """ Checks if the property with the given name is set, if not, use default
         """
         global defaults
-        if name in self.properties.keys():
-            return self.properties[name]
+        if name in self.properties:
+            return self.compileProperty(self.properties[name])
         else:
-            return defaults[name]
+            return self.compileProperty(defaults[name])
         
     def __getitem__(self, item):
         return self.get(item)
 
     def __setitem__(self, key, value):
         self.properties[key] = value
-        
+
+    def compileProperty(self, value):
+        if not isinstance(value, basestring):
+            return value
+        matches = single_br_pattern.findall(value)
+        for match in matches:
+            key = match[1:-1] # "{document}" --> "document"
+            try:
+                value = value.replace(match, self[key])
+            except KeyError:
+                pass # don't replace, probably just text
+        return value
+
     def hyperlink(self, shown_text, link_to, **args):
         """ Returns a hyperlink (<a> tag) according to website's settings
         """
         # fix hyperlink if settings are set
         link_to = self.updateLink(link_to)
-        link_code = self.get('link_code')
+        link_code = self['link_code']
         return link_code.format(link_to=link_to,
                                 shown_text=shown_text, **args)
     
@@ -49,16 +63,17 @@ class Website(object):
             Parameters used:
             - extend_relative: changes relative links into absolute links
         """
-        if self.get('expand_relative') and isRelative(link):
+        if self['expand_relative'] and isRelative(link):
             return os.path.join(self.properties['absolute_path'], link)
         return link
 
 
-    def navigation(self, links):
+    def navigation(self):
         """ Returns the navigation of the website
         """
         nav_code = self.get('nav_code')
         nav_element = self.get('nav_element')
+        links = self.get('nav_links')
         all_nav_elements = [nav_element.format(nav_link=self.hyperlink(l[0], l[1]))
                             for l in links]
         return nav_code.format(nav_elements="\n\t".join(all_nav_elements))
@@ -101,3 +116,23 @@ class Website(object):
 
 def isRelative(link):
     raise NotImplementedError("Website.isRelative has not been implemented yet")
+
+
+# Test of some of the properties
+website = Website()
+
+# simple text, nothing suprising
+print "code_about:"
+print website['code_about']
+print ""
+# non-text attribute
+print "expand_relative:"
+print website['expand_relative']
+print ""
+# text that needs one level of compilation
+print "page_footer:"
+print website['page_footer']
+print ""
+# text that needs multiple levels of compilation
+print "head_declarations:"
+print website['head_declarations']
